c9ea7b71e1f4a59241c8c2f181e9d3140fbfcb56
royaljuggy <dragonte@hotmail.ca>
2020-12-28 19:40:03 -0500

    Surprise button updated

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 var btnSurprise = document.getElementById("surpriseButton")
-var pDisclaimers = document.getElementById("disclaimers") // paragraph tag of disclaimers
+var pSurprise = document.getElementById("surpriseText") // paragraph tag of disclaimers
 
 // Update disclaimer text
 btnSurprise.addEventListener("click", function(){
-    pDisclaimers.textContent += " Update: One new Emilia fan found!"
+    pSurprise.style.visibility = "visible"
     btnSurprise.style.visibility = "hidden"
 })
 
