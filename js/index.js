4388a50185736f081f62bcb7ad70d2962e9edbec
royaljuggy <dragonte@hotmail.ca>
2020-12-31 11:15:51 -0500

    Justified paragraphs, added margins

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 let btnSurprise = document.getElementById("surpriseButton")
-let pSurprise = document.getElementById("surpriseText") // paragraph tag of disclaimers
+let pSurprise = document.getElementById("disclaimers") // paragraph tag of disclaimers
 
 // Update disclaimer text
 btnSurprise.addEventListener("click", function(){
-    pSurprise.style.visibility = "visible"
-    btnSurprise.style.visibility = "hidden"
+    pSurprise.innerHTML += " <strong>Update: One new Emilia fan found!</strong>"
+    btnSurprise.style.visibility = 'hidden'
 })
 
