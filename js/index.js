8427fc1cf7d05bb69008b6f4b8f486c66c4a319b
royaljuggy <dragonte@hotmail.ca>
2021-01-02 17:27:16 -0500

    Margin consistency added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-let btnSurprise = document.getElementById("surpriseButton")
-let pSurprise = document.getElementById("disclaimers") // paragraph tag of disclaimers
+const btnSurprise = document.getElementById("surpriseButton")
+const pSurprise = document.getElementById("disclaimers") // paragraph tag of disclaimers
+const quizForm = document.getElementById("quiz")
 
 // Update disclaimer text
 btnSurprise.addEventListener("click", function(){
-    pSurprise.innerHTML += " <strong>Update: One new Emilia fan found!</strong>"
-    btnSurprise.style.visibility = 'hidden'
+    pSurprise.innerHTML += " Update: One new Emilia fan found!".bold()
+    btnSurprise.style.display = 'none'
+    btnSurprise.remove()
 })
 
+function handleForm(event) {
+    event.preventDefault()
+}
+
+quizForm.addEventListener('submit', handleForm);
